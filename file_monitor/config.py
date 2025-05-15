5d76c8be3cd57e6ed86edd85a8de73cb66f00f17
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-15 12:57:32 +0300

    🐛 fix(config): update default Docker Compose path
    - Changed `DOCKER_COMPOSE_PATH` default value to "docker-compose.yml" instead of "../docker-compose.yml".
    - Aligns with expected local setup and prevents potential path resolution issues.

--- file_monitor/config.py
+++ file_monitor/config.py
@@ -12,5 +12,5 @@ PROMPT_TIMEOUT = int(os.getenv("PROMPT_TIMEOUT", 120))
 LOG_FILE = os.getenv("LOG_FILE", "./rebuild_log.txt")
 HASH_DB_FILE = os.getenv("HASH_DB_FILE", "file_tracker.db")
 SPECIAL_FILES = os.getenv("SPECIAL_FILES", "api.R,api-wrapper-orig.R").split(",")
-DOCKER_COMPOSE_PATH = os.getenv("DOCKER_COMPOSE_PATH", "../docker-compose.yml")  # One directory up
+DOCKER_COMPOSE_PATH = os.getenv("DOCKER_COMPOSE_PATH", "docker-compose.yml")  # One directory up
 FILE_EXTENSIONS = os.getenv("FILE_EXTENSIONS", ".R").split(",")  # List of file extensions to track
